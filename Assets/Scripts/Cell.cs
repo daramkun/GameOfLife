49cd62e00d659dbc94da702a0da2ed8398c890b9
진재연(Jin Jae-yeon) <daramkun@live.com>
2020-04-25 13:53:29 +0900

    Initial commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour
+{
+    public int X;
+    public int Y;
+    public bool Alive;
+
+    CellGrid cellGrid;
+    Image image;
+
+    void Awake ()
+    {
+        cellGrid = transform.parent.GetComponent<CellGrid> ();
+        image = GetComponent<Image> ();
+    }
+
+    void Update ()
+    {
+        image.color = Alive ? Color.white : new Color (0.2f, 0.2f, 0.2f);
+    }
+
+    public void Toggle()
+    {
+        if (cellGrid.Playing)
+            return;
+        Alive = !Alive;
+        cellGrid.Grid [X, Y] = Alive;
+    }
+}
---
+++ Assets/Scripts/CellGrid.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellGrid : MonoBehaviour
+{
+	public GameObject CellPrefab;
+
+	public int Width = 10, Height = 10;
+
+	public bool Playing = false;
+
+	public GameObject PlayButton, PauseButton;
+
+	GameObject [,] Objects;
+	public bool [,] Grid;
+	bool[,] BackGrid;
+
+	void Awake ()
+	{
+		Refresh ();
+	}
+
+	public void InputWidth (string width)
+	{
+		Width = int.Parse (width);
+	}
+
+	public void InputHeight (string height)
+	{
+		Height = int.Parse (height);
+	}
+
+	public void Refresh ()
+	{
+		Playing = false;
+
+		foreach (Transform child in transform)
+			Destroy (child.gameObject);
+
+		Objects = new GameObject [Width, Height];
+		Grid = new bool [Width, Height];
+		BackGrid = new bool [Width, Height];
+		GetComponent<GridLayoutGroup> ().constraintCount = Width;
+		for (int y = 0; y < Height; ++y)
+		{
+			for (int x = 0; x < Width; ++x)
+			{
+				GameObject cell = Instantiate (CellPrefab, transform);
+				Cell cellComp = cell.GetComponent<Cell> ();
+				cellComp.X = x;
+				cellComp.Y = y;
+				cellComp.Alive = false;
+				Objects [x, y] = cell;
+			}
+		}
+	}
+
+	public void Play ()
+	{
+		Playing = true;
+	}
+
+	public void Stop ()
+	{
+		Playing = false;
+	}
+
+	void FixedUpdate ()
+	{
+		PlayButton.GetComponent<Button> ().enabled = !Playing;
+		PauseButton.GetComponent<Button> ().enabled = Playing;
+
+		if (Playing)
+		{
+			for (int y = 0; y < Height; ++y)
+			{
+				for (int x = 0; x < Width; ++x)
+				{
+					bool isAlive = Grid [x, y];
+					int count = 0;
+					for (int yy = -1; yy <= 1; ++yy)
+					{
+						for(int xx = -1; xx <= 1; ++xx)
+						{
+							if (xx == 0 && yy == 0)
+								continue;
+							count += __GetValueFromGrid (Grid, x + xx, y + yy, Width, Height);
+						}
+					}
+
+					if (isAlive)
+					{
+						BackGrid [x, y] = (count == 2 || count == 3);
+					}
+					else
+					{
+						BackGrid [x, y] = count == 3;
+					}
+				}
+			}
+
+			SwapBuffer ();
+		}
+	}
+
+	int __GetValueFromGrid (bool [,] grid, int x, int y, int width, int height)
+	{
+		if (x < 0 || y < 0) return 0;
+		if (x >= width || y >= height) return 0;
+		return grid [x, y] ? 1 : 0;
+	}
+
+	public void SwapBuffer()
+	{
+		for(int y = 0; y < Height; ++y)
+		{
+			for(int x = 0; x < Width; ++x)
+			{
+				Objects [x, y].GetComponent<Cell> ().Alive = BackGrid [x, y];
+			}
+		}
+
+		bool [,] temp = Grid;
+		Grid = BackGrid;
+		BackGrid = temp;
+	}
+}
